d278854f13a63069641971418c5710dcdaa2e254
Adama-Umaru <adamaumaru@gmail.com>
2023-05-30 09:22:06 +0100

    first commit

---
+++ questions_pdf_generator.py
@@ -0,0 +1,47 @@
+import requests
+import pdfkit
+
+endpoint ="https://api.stackexchange.com/2.3/questions?site=stackoverflow&pagesize=50&sort=votes"
+num_questions = 50
+
+def fetch_questions(endpoint, num_questions):
+    response = requests.get(endpoint)
+    data = response.json()
+    questions = data["items"][:num_questions]
+    return questions
+
+def create_pdf(questions):
+    html_content = "<html><head><style>img {max-width: 50%;}</style></head><body>"
+    question_number = 1
+
+    for question in questions:
+        title = question["title"]
+        views = question["view_count"]
+        link = question["link"]
+
+        # Check if the "imageURL" key exists in the dictionary
+        if "imageURL" in question:
+            image_url = question["imageURL"]
+        else:
+            image_url = ""  # Set a default value if the key is missing
+
+        html_content += f"<h2>{question_number}. {title}</h2>"
+        html_content += f"<p>Views: {views}</p>"
+        html_content += f'<a href="{link}">{link}</a>'
+        html_content += f'<img src="{image_url}"><br><br>'
+
+        question_number += 1
+
+    html_content += "</body></html>"
+    
+
+    
+    config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+    pdfkit.from_string(html_content, "questions.pdf", configuration=config)
+
+
+if __name__ == "__main__":
+    endpoint = input("Enter the endpoint URL: ")
+    questions = fetch_questions(endpoint, num_questions)
+    create_pdf(questions)
+    print("PDF created successfully.")
